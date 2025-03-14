b73ff2ca14c7c8cd3d783224caa01765dec22113
samuelgbenga <samuelgbenga972@gmail.com>
2025-03-14 02:45:21 +0100

    first commit

---
+++ discover-server/src/main/java/com/fxsimulator/discovery-server/App.java
@@ -0,0 +1,13 @@
+package com.fxsimulator.discovery-server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
